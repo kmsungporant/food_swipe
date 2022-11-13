0a45d48677db1145563c0cc6808b261e155c592c
kmsungporant <kmsungporant@gmail.com>
2022-11-12 22:54:49 -0500

    11/12

--- pages/api/Testing.tsx
+++ pages/api/Testing.tsx
@@ -3,6 +3,10 @@ import { Restaurant } from "./types";
 import axios from 'axios'
 
 export default function Testing() {
+
+    const [details, setDetails] = useState<String[]>([])
+
+    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key=YOUR_API_KEY`
   
     return (
         <div>
