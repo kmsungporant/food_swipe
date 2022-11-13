7893447f9c5fa61d8733bcd078535729cdfefffd
Dan Le <tle57@gmu.edu>
2022-11-13 00:19:18 -0500

    fix cache

--- components/foodCard/foodCard.tsx
+++ components/foodCard/foodCard.tsx
@@ -1,38 +1,55 @@
-import { BiDollar } from "react-icons/bi"
+import { BiDollar } from "react-icons/bi";
 
 export default function FoodCard({ restaurant }: { restaurant: any }) {
+	function getImage() {
+		return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=${restaurant.photos![0]?.width}&photo_reference=${restaurant?.photos![0]?.photo_reference}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`;
+	}
 
-    function getImage() {
-        return (`https://maps.googleapis.com/maps/api/place/photo?maxwidth=${restaurant.photos![0].width}&photo_reference=${restaurant.photos![0].photo_reference}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`);
-    }
-
-
-    return (
-        <>
-            <div className="">
-                <div className="h-full w-full bg-black flex flex-col rounded-3xl p-3 gap-y-2 ">
-                    <span className="text-3xl font-black flex justify-center ">{restaurant.name}</span>
-                    <div className=" flex justify-start w-full overflow-hidden rounded-3xl">
-                        <img draggable="false" height="100%" width="100%" src={getImage()} />
-                    </div>
-                    <span>
-                        Now {restaurant.opening_hours.open_now ? <span className="text-green-700 font-black underline">Opened</span> : <span className="text-red-700 font-black underline">Closed</span>}
-                    </span>
-                    <span>
-                        Rating: {restaurant.rating} out of 5
-                    </span>
-                    <span className="text-xl flex flex-row text-yellow-500">
-                        {Array(restaurant.price_level).fill(<BiDollar className="-ml-1" />)}
-                    </span>
-                    <div className="flex justify-between flex-col ">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, atque ipsam vel magnam ipsa sequi iusto saepe? Quisquam, excepturi voluptatem, voluptate debitis repellat fugiat eaque itaque minima esse deserunt consequatur.</p>
-                        <span>
-                        </span>
-                    </div>
-                </div>
-
-            </div>
-
-        </>
-    )
+	return (
+		<>
+			<div className="">
+				<div className="h-full w-full bg-black flex flex-col rounded-3xl p-3 gap-y-2 ">
+					<span className="text-3xl font-black flex justify-center ">
+						{restaurant.name}
+					</span>
+					<div className=" flex justify-start w-full overflow-hidden rounded-3xl">
+						<img
+							draggable="false"
+							height="100%"
+							width="100%"
+							src={getImage()}
+						/>
+					</div>
+					<span>
+						Now{" "}
+						{restaurant?.opening_hours?.open_now ? (
+							<span className="text-green-700 font-black underline">
+								Opened
+							</span>
+						) : (
+							<span className="text-red-700 font-black underline">
+								Closed
+							</span>
+						)}
+					</span>
+					<span>Rating: {restaurant.rating} out of 5</span>
+					<span className="text-xl flex flex-row text-yellow-500">
+						{Array(restaurant.price_level).fill(
+							<BiDollar className="-ml-1" />
+						)}
+					</span>
+					<div className="flex justify-between flex-col ">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Ratione, atque ipsam vel magnam ipsa sequi
+							iusto saepe? Quisquam, excepturi voluptatem,
+							voluptate debitis repellat fugiat eaque itaque
+							minima esse deserunt consequatur.
+						</p>
+						<span></span>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,224 @@
-import { motion } from "framer-motion"
-import FoodCard from "../components/foodCard/foodCard"
-import Footer from "../components/footer"
-import Image from 'next/image'
+import { motion } from "framer-motion";
+import FoodCard from "../components/foodCard/foodCard";
+import Footer from "../components/footer";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Restaurant } from "./api/types";
 
-export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, setMainTitle: any }) {
-  // Coordinates (coordinates[0] = latitude, coordinates[1] = longitude)
-  const [coordinates, setCoordinates] = useState<Number[]>([]);
-  // Restaurants
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-  const [currentCard, setCurrentCard] = useState(0);
-  const [currRestaurants, setCurrRestaurants] = useState(restaurants.slice(0, 3));
+export default function Home({
+	mainTitle,
+	setMainTitle,
+}: {
+	mainTitle: boolean;
+	setMainTitle: any;
+}) {
+	// Coordinates (coordinates[0] = latitude, coordinates[1] = longitude)
+	const [coordinates, setCoordinates] = useState<Number[]>([]);
+	// Restaurants
+	const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+	const [currRestaurants, setCurrRestaurants] = useState<Restaurant[]>(
+		restaurants.slice(0, 3)
+	);
+	// Boolean
+	const [render, setRender] = useState<boolean>(false);
 
-  //const [details, setDetails] = useState<Restaurant[]>([]);
+	//const [details, setDetails] = useState<Restaurant[]>([]);
 
+	// Axios config
+	var axios = require("axios");
+	var config = {
+		method: "get",
+		url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0]}%2C${coordinates[1]}&radius=1000&type=restaurant&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
+	};
 
-  // Axios config
-  var axios = require("axios");
-  var config = {
-    method: "get",
-    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0]}%2C${coordinates[1]}&radius=1600&type=restaurant&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
-  };
+	// Fetches data from Places API and stores restaurant data.
+	useEffect(() => {
+		if (coordinates[0] != null) {
+			console.log("Latitude is " + coordinates[0]);
+			console.log("Longitude is " + coordinates[1]);
+			console.log(currRestaurants);
+			console.log(restaurants);
+			// Calls API
+			axios(config)
+				.then(function (response: any) {
+					// Stores restaurant data
+					setRestaurants(response?.data?.results);
+				})
+				.catch(function (error: any) {
+					console.log(error);
+				});
+		}
+	}, [coordinates]);
 
-  // Fetches data from Places API and stores restaurant data.
-  useEffect(() => {
-    if (coordinates[0] != null) {
-      console.log("Latitude is " + coordinates[0]);
-      console.log("Longitude is " + coordinates[1]);
-      console.log(currRestaurants);
-      // Calls API
-      axios(config)
-        .then(function (response: any) {
-          // Stores restaurant data
-          setRestaurants(response?.data?.results);
-        })
-        .catch(function (error: any) {
-          console.log(error);
-        });
-    }
-  }, [coordinates]);
+	useEffect(() => {
+		if (restaurants[0] != null) {
+			setCurrRestaurants(restaurants.slice(0, 3));
+			setRender(true);
+			console.log("Rendered");
+		}
+	}, [restaurants]);
 
-  useEffect(() => {
-    setCurrRestaurants(restaurants.slice(0, 3));
+	// On mount, ask for user coordinates
+	useEffect(() => {
+		if (coordinates[0] == null) {
+			// Gets lat and long coordinates
+			navigator.geolocation.getCurrentPosition((position) => {
+				setCoordinates([
+					position.coords.latitude,
+					position.coords.longitude,
+				]);
+			});
+		}
+	}, []);
 
-  }, [restaurants])
+	// Handle food card
 
-  // On mount, ask for user coordinates
-  useEffect(() => {
-    if (coordinates[0] == null) {
-      // Gets lat and long coordinates
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCoordinates([
-          position.coords.latitude,
-          position.coords.longitude,
-        ]);
-      });
-
-    }
-  }, []);
-
-  // Returns image from restaurant[index]
-  
-
-
-  return (
-    <>
-      <motion.div initial={{ opacity: 1 }} animate={mainTitle ? {} : { opacity: 0 }}>
-        <Image draggable="false" src="/main.jpg" alt="main picture" layout="fill" objectFit="cover" objectPosition="center" />
-      </motion.div>
-      {/* <motion.div initial={{ opacity: 1 }} animate={mainTitle ? { opacity: 0 } : { opacity: 1 }}>
+	return (
+		<>
+			<motion.div
+				initial={{ opacity: 1 }}
+				animate={mainTitle ? {} : { opacity: 0 }}
+			>
+				<Image
+					draggable="false"
+					src="/main.jpg"
+					alt="main picture"
+					layout="fill"
+					objectFit="cover"
+					objectPosition="center"
+				/>
+			</motion.div>
+			{/* <motion.div initial={{ opacity: 1 }} animate={mainTitle ? { opacity: 0 } : { opacity: 1 }}>
         <Image src="/backgroundCard.jpg" alt="background card picture" layout="fill" objectFit="cover" objectPosition="center" />
       </motion.div> */}
-      <div className="relative z-10 h-full min-h-full  transition-colors text-white" >
-        <div className="flex relative items-center justify-center h-full">
-          <div className="absolute">
-            <div className="flex flex-col items-center justify-center w-full h-full">
-              <div className="flex flex-col w-4/5 font-serif text-6xl font-black text-right">
-                <motion.span initial={{ opacity: 1, zIndex: 50 }} animate={mainTitle ? "" : { opacity: 0, zIndex: 0, x: -1000, transition: { ease: "easeInOut", delay: 0 } }}>Food</motion.span>
-                <motion.span initial={{ opacity: 1, zIndex: 50 }} animate={mainTitle ? "" : { opacity: 0, zIndex: 0, x: -1000, transition: { ease: "easeInOut", delay: 0.05 } }}>Swipe</motion.span>
-                <motion.span initial={{ opacity: 1, zIndex: 50 }} animate={mainTitle ? "" : { opacity: 0, zIndex: 0, x: -1000, transition: { ease: "easeInOut", delay: 0.1 } }} className="mt-5 text-xl">
-                  A food-based website built to expand your palette.
-                </motion.span>
-                <motion.div initial={{ opacity: 1, zIndex: 50 }} animate={mainTitle ? "" : { opacity: 0, zIndex: 0, x: -1000, transition: { ease: "easeInOut", delay: 0.15 } }} className="flex justify-end w-full mt-5">
-                  <button className="flex justify-center p-3 text-lg text-black bg-white rounded-full w-36" onClick={() => { setMainTitle(false) }}>
-                    Begin
-                  </button>
-                </motion.div>
-              </div>
-            </div>
-          </div>
-          <div className="absolute w-2/5">
-            <motion.div initial={{ opacity: 0, zIndex: 0 }} animate={mainTitle ? "" : { opacity: 1, zIndex: 50, transition: { delay: 0.5 } }} >
-              {currRestaurants.map((restaurant, i) => (
-                <motion.div drag="x"
-                  dragConstraints={{ left: -150, right: 150 }}
-                  dragSnapToOrigin={true}
-                  dragElastic={0.25}
-                  dragTransition={{ bounceStiffness: 100 }}
-                  initial={{ scale: 1.0 }} key={i} className="absolute flex justify-center items-center h-full w-full">
-                  <FoodCard restaurant={restaurant} />
-                </motion.div>
-              ))}
-            </motion.div>
-
-          </div>
-        </div>
-        {mainTitle ? <></> : <Footer />}
-      </div>
-
-    </>
-
-  )
+			<div className="relative z-10 h-full min-h-full  transition-colors text-white">
+				<div className="flex relative items-center justify-center h-full">
+					<div className="absolute">
+						<div className="flex flex-col items-center justify-center w-full h-full">
+							<div className="flex flex-col w-4/5 font-serif text-6xl font-black text-right">
+								<motion.span
+									initial={{ opacity: 1, zIndex: 50 }}
+									animate={
+										mainTitle
+											? ""
+											: {
+													opacity: 0,
+													zIndex: 0,
+													x: -1000,
+													transition: {
+														ease: "easeInOut",
+														delay: 0,
+													},
+											  }
+									}
+								>
+									Food
+								</motion.span>
+								<motion.span
+									initial={{ opacity: 1, zIndex: 50 }}
+									animate={
+										mainTitle
+											? ""
+											: {
+													opacity: 0,
+													zIndex: 0,
+													x: -1000,
+													transition: {
+														ease: "easeInOut",
+														delay: 0.05,
+													},
+											  }
+									}
+								>
+									Swipe
+								</motion.span>
+								<motion.span
+									initial={{ opacity: 1, zIndex: 50 }}
+									animate={
+										mainTitle
+											? ""
+											: {
+													opacity: 0,
+													zIndex: 0,
+													x: -1000,
+													transition: {
+														ease: "easeInOut",
+														delay: 0.1,
+													},
+											  }
+									}
+									className="mt-5 text-xl"
+								>
+									A food-based website built to expand your
+									palette.
+								</motion.span>
+								<motion.div
+									initial={{ opacity: 1, zIndex: 50 }}
+									animate={
+										mainTitle
+											? ""
+											: {
+													opacity: 0,
+													zIndex: 0,
+													x: -1000,
+													transition: {
+														ease: "easeInOut",
+														delay: 0.15,
+													},
+											  }
+									}
+									className="flex justify-end w-full mt-5"
+								>
+									<button
+										className="flex justify-center p-3 text-lg text-black bg-white rounded-full w-36"
+										onClick={() => {
+											setMainTitle(false);
+										}}
+									>
+										Begin
+									</button>
+								</motion.div>
+							</div>
+						</div>
+					</div>
+					<div className="absolute w-2/5">
+						<motion.div
+							initial={{ opacity: 0, zIndex: 0 }}
+							animate={
+								mainTitle
+									? ""
+									: {
+											opacity: 1,
+											zIndex: 50,
+											transition: { delay: 0.5 },
+									  }
+							}
+						>
+							{render
+								? restaurants?.map((restaurant) => (
+										<motion.div
+											drag="x"
+											dragConstraints={{
+												left: -150,
+												right: 150,
+											}}
+											dragSnapToOrigin={true}
+											dragElastic={0.25}
+											dragTransition={{
+												bounceStiffness: 100,
+											}}
+											initial={{ scale: 1.0 }}
+											className="absolute flex justify-center items-center h-full w-full"
+										>
+											<FoodCard restaurant={restaurant} />
+										</motion.div>
+								  ))
+								: null}
+						</motion.div>
+					</div>
+				</div>
+				{mainTitle ? <></> : <Footer />}
+			</div>
+		</>
+	);
 }
