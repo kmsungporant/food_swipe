c9bbea1891903d2702a83de9fc9f117558430063
peterc03 <peterc03@vt.edu>
2022-11-13 03:28:45 -0500

    initial commit

--- components/foodCard/foodCard.tsx
+++ components/foodCard/foodCard.tsx
@@ -1,6 +1,6 @@
 import { BiDollar } from "react-icons/bi"
 
-export default function FoodCard({ restaurant }: { restaurant: any }) {
+export default function FoodCard({ restaurant }: { restaurant: any }, { information } : { information :any }) {
 
     function getImage() {
         return (`https://maps.googleapis.com/maps/api/place/photo?maxwidth=${restaurant.photos![0].width}&photo_reference=${restaurant.photos![0].photo_reference}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`);
@@ -10,23 +10,24 @@ export default function FoodCard({ restaurant }: { restaurant: any }) {
     return (
         <>
             <div className="">
-                <div className="h-full w-full bg-black flex flex-col rounded-3xl p-3 gap-y-2 ">
-                    <span className="text-3xl font-black flex justify-center ">{restaurant.name}</span>
-                    <div className=" flex justify-start w-full overflow-hidden rounded-3xl">
+                <div className="flex flex-col w-full h-full p-3 bg-black rounded-3xl gap-y-2 ">
+                    <span className="flex justify-center text-3xl font-black ">{restaurant.name}</span>
+                    <div className="flex justify-start w-full overflow-hidden rounded-3xl">
                         <img draggable="false" height="100%" width="100%" src={getImage()} />
                     </div>
                     <span>
-                        Now {restaurant.opening_hours.open_now ? <span className="text-green-700 font-black underline">Opened</span> : <span className="text-red-700 font-black underline">Closed</span>}
+                        Now {restaurant.opening_hours.open_now ? <span className="font-black text-green-700 underline">Opened</span> : <span className="font-black text-red-700 underline">Closed</span>}
                     </span>
                     <span>
                         Rating: {restaurant.rating} out of 5
                     </span>
-                    <span className="text-xl flex flex-row text-yellow-500">
+                    <span className="flex flex-row text-xl text-yellow-500">
                         {Array(restaurant.price_level).fill(<BiDollar className="-ml-1" />)}
                     </span>
-                    <div className="flex justify-between flex-col ">
+                    <div className="flex flex-col justify-between ">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, atque ipsam vel magnam ipsa sequi iusto saepe? Quisquam, excepturi voluptatem, voluptate debitis repellat fugiat eaque itaque minima esse deserunt consequatur.</p>
                         <span>
+                            
                         </span>
                     </div>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/footer"
 import Image from 'next/image'
 import { useEffect, useState } from "react";
 import { Restaurant } from "./api/types";
+import { Information } from "./api/type2";
 
 export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, setMainTitle: any }) {
   // Coordinates (coordinates[0] = latitude, coordinates[1] = longitude)
@@ -13,8 +14,8 @@ export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, 
   const [currentCard, setCurrentCard] = useState(0);
   const [currRestaurants, setCurrRestaurants] = useState(restaurants.slice(0, 3));
 
-  //const [details, setDetails] = useState<Restaurant[]>([]);
-
+  const [details, setDetails] = useState<String[]>([]);
+  const [informations, setInformations] = useState<Information>();
 
   // Axios config
   var axios = require("axios");
@@ -23,12 +24,12 @@ export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, 
     url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0]}%2C${coordinates[1]}&radius=1600&type=restaurant&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
   };
 
+
   // Fetches data from Places API and stores restaurant data.
   useEffect(() => {
     if (coordinates[0] != null) {
       console.log("Latitude is " + coordinates[0]);
       console.log("Longitude is " + coordinates[1]);
-      console.log(currRestaurants);
       // Calls API
       axios(config)
         .then(function (response: any) {
@@ -60,9 +61,30 @@ export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, 
     }
   }, []);
 
-  // Returns image from restaurant[index]
-  
+  var axios2 = require("axios");
+  var config2 = {
+    method: "get",
+    url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${restaurants![0]?.place_id}&fields=editorial_summary,website,formatted_phone_number&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
+  };
 
+  useEffect(
+    () => {
+        axios2(config2)
+          .then(function (response: any) {
+            setInformations(response?.data?.result);
+          })
+          .catch(function (error: any) {
+            console.log(error);
+          });
+      
+    }, []);
+
+    useEffect(() => {
+      if (informations != null) {
+        console.log(informations);
+      }
+      
+    }, [informations])
 
   return (
     <>
@@ -72,8 +94,8 @@ export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, 
       {/* <motion.div initial={{ opacity: 1 }} animate={mainTitle ? { opacity: 0 } : { opacity: 1 }}>
         <Image src="/backgroundCard.jpg" alt="background card picture" layout="fill" objectFit="cover" objectPosition="center" />
       </motion.div> */}
-      <div className="relative z-10 h-full min-h-full  transition-colors text-white" >
-        <div className="flex relative items-center justify-center h-full">
+      <div className="relative z-10 h-full min-h-full text-white transition-colors" >
+        <div className="relative flex items-center justify-center h-full">
           <div className="absolute">
             <div className="flex flex-col items-center justify-center w-full h-full">
               <div className="flex flex-col w-4/5 font-serif text-6xl font-black text-right">
@@ -94,11 +116,12 @@ export default function Home({ mainTitle, setMainTitle }: { mainTitle: boolean, 
             <motion.div initial={{ opacity: 0, zIndex: 0 }} animate={mainTitle ? "" : { opacity: 1, zIndex: 50, transition: { delay: 0.5 } }} >
               {currRestaurants.map((restaurant, i) => (
                 <motion.div drag="x"
+
                   dragConstraints={{ left: -150, right: 150 }}
                   dragSnapToOrigin={true}
                   dragElastic={0.25}
                   dragTransition={{ bounceStiffness: 100 }}
-                  initial={{ scale: 1.0 }} key={i} className="absolute flex justify-center items-center h-full w-full">
+                  initial={{ scale: 1.0 }} key={i} className="absolute flex items-center justify-center w-full h-full">
                   <FoodCard restaurant={restaurant} />
                 </motion.div>
               ))}
