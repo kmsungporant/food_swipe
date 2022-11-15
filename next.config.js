c72cfe3724855f901c5d850f45c26a9381c601a5
Dan Le <tle57@gmu.edu>
2022-11-15 13:47:23 -0500

    test

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ module.exports = {
 				source: "/api/:path*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://food-swipe-kmsungporant.vercel.app/",
+					},
 					{
 						key: "Access-Control-Allow-Methods",
 						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
