59384a9fab5e1d51a2c5ba763de48cef2b6f2d34
Dan Le <tle57@gmu.edu>
2022-11-15 14:05:37 -0500

    testonemoretime

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		return [
 			{
 				// matching all API routes
-				source: "/:path*",
+				source: "*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{
